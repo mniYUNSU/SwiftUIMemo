d66db3a8df0f3fc36103ec5cf12e56272d591d3e
Yunsu Bae <ownsgks@gmail.com>
2023-07-21 18:21:24 +0900

    add edit memo

--- SwiftUIMemo/View/ComposeView.swift
+++ SwiftUIMemo/View/ComposeView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ComposeView: View {
     @EnvironmentObject var store: MemoStore
     
+    var memo: Memo? = nil
+    
     @Environment(\.dismiss) var dismiss
     
     @State private var content: String = ""
@@ -17,9 +19,13 @@ struct ComposeView: View {
     var body: some View {
         NavigationView {
             VStack {
-                TextEditor(text: $content).padding()
+                TextEditor(text: $content).padding().onAppear {
+                    if let memo = memo {
+                        content = memo.content
+                    }
+                }
             }
-            .navigationTitle("새로운 메모")
+            .navigationTitle(memo != nil ? "메모 편집" : "새로운 메모")
             .navigationBarTitleDisplayMode(.inline)
             .toolbar {
                 ToolbarItemGroup(placement: .navigationBarLeading) {
@@ -31,7 +37,11 @@ struct ComposeView: View {
                 }
                 ToolbarItemGroup(placement: .navigationBarTrailing) {
                     Button {
-                        store.insert(memo: content)
+                        if let memo = memo {
+                            store.update(memo: memo, content: content)
+                        } else {
+                            store.insert(memo: content)
+                        }
                         dismiss()
                     } label: {
                         Text("저장")
